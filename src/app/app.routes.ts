3f50bf9e841b82b6869b73afa01b8781fa779b4b
zterezra <subadaga@me.com>
2024-03-18 13:23:27 -0600

    Add linting capabilities and header component
    - Adds linting and prettier functionality to the project configuration
    - The HeaderComponent, its corresponding tests, and style files have been generated for use in the project.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { FormsComponent } from './forms/forms.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/forms', pathMatch: 'full' },
+  { path: 'forms', component: FormsComponent },
+];
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { HeaderComponent } from './components/header/header.component';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatIcon } from '@angular/material/icon';
+import { MatIconButton } from '@angular/material/button';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HeaderComponent],
+  exports: [HeaderComponent],
+  imports: [CommonModule, MatToolbar, MatIcon, MatIconButton],
 })
-export class CoreModule { }
+export class CoreModule {}
